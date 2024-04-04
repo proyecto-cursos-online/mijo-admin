fd8a8a7fd69e5509307eeefe40070dc18df5da52
GabrielMCC2020 <gabrielmcc2002cultural@gmail.com>
2024-04-04 07:44:34 -0500

    Formulario del registro de curso II

--- src/app/modules/course/course-add/course-add.component.ts
+++ src/app/modules/course/course-add/course-add.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-
+import { CourseService } from '../service/course.service';
 @Component({
   selector: 'app-course-add',
   templateUrl: './course-add.component.html',
   styleUrls: ['./course-add.component.scss']
 })
 export class CourseAddComponent {
-  constructor() { }
+
+  subcategories:any = [];
+  subcategories_back:any = [];
+  categories:any = [];
+  instructores:any = [];
+
+  isLoading:any;  
+  constructor(
+    public courseService: CourseService,
+  ) { }
 
   ngOnInit(): void {
+    this.isLoading = this.courseService.isLoading$;
+    this.courseService.lisConfig().subscribe((resp:any) => {
+      console.log(resp);
+      this.subcategories = resp.subcategories;
+      this.categories = resp.categories;
+      this.instructores = resp.instructores;
+    })
+  }
+
+  selectCategorie(event:any){
+    let VALUE = event.target.value;
+    console.log(VALUE);
+    this.subcategories_back = this.subcategories.filter((item:any) => item.categorie_id == VALUE);
+    
   }
 
   save() {
--- src/app/modules/course/service/course.service.ts
+++ src/app/modules/course/service/course.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../../auth';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
+
+  constructor(
+    private http: HttpClient,
+    public authservice: AuthService,
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  lisConfig(){
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authservice.token});
+    let URL = URL_SERVICIOS+"/course/config";
+    this.isLoadingSubject.next(true);
+    return this.http.get(URL,{headers: headers}).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    );
+  }
 }
